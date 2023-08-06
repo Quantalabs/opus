59f77d1038206fcaf11e5cde99447679def1ec25
Quantalabs <55121845+Quantalabs@users.noreply.github.com>
2023-08-05 20:59:47 -0700

    build: change from tsc and ncc to ncc

--- dist/index.js
+++ dist/index.js
@@ -1,14 +1,2 @@
-/******/ (() => { // webpackBootstrap
-/******/ 	"use strict";
-/******/ 	/* webpack/runtime/compat */
-/******/ 	
-/******/ 	if (typeof __nccwpck_require__ !== 'undefined') __nccwpck_require__.ab = __dirname + "/";
-/******/ 	
-/************************************************************************/
-var __webpack_exports__ = {};
-
-console.log("Hello World");
+require('./sourcemap-register.js');(()=>{if(typeof __nccwpck_require__!=="undefined")__nccwpck_require__.ab=__dirname+"/";var e={};console.log("Hello World");module.exports=e})();
 //# sourceMappingURL=index.js.map
-module.exports = __webpack_exports__;
-/******/ })()
-;
