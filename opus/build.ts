981c76180e3394b4dcb8f399fce201e7b3562f81
Quantalabs <55121845+Quantalabs@users.noreply.github.com>
2023-11-22 18:22:21 -0800

    feat: build pdfs

---
+++ opus/build.ts
@@ -0,0 +1,190 @@
+import chalk from "chalk"
+import { jsPDF } from "jspdf"
+import convert from "convert-units"
+
+function createDocument(element: Object): Object {
+    if (element["content"] != "") {
+        console.warn(
+            chalk.yellow(
+                "Warning: document content is not empty. This content will be ignored."
+            )
+        )
+    }
+
+    element["options"]["numberPages"] =
+        element["numberPages"] == "true" ? true : false
+
+    if (
+        !element["options"]["numberPages"] &&
+        (element["options"]["numXPos"] != null ||
+            element["options"]["numYPos"] != null)
+    ) {
+        console.warn(
+            chalk.yellow(
+                "Warning: numberPages is set to false in document options, but numXPos and/or numYPos is set. Page numbers will not be shown unless specified in page-specific options."
+            )
+        )
+    }
+
+    if (
+        element["options"]["color"] &&
+        !element["options"]["color"].startsWith("#")
+    ) {
+        console.warn(
+            chalk.yellow(
+                "Warning: document color is not a valid hex color. It will be set to default (#FFFFFF)."
+            )
+        )
+        element["options"]["color"] = "#FFFFFF"
+    }
+
+    if (
+        element["options"]["orientation"] != "portrait" &&
+        element["options"]["orientation"] != "landscape"
+    ) {
+        console.warn(
+            chalk.yellow(
+                "Warning: document orientation is not valid. It will be set to default (portrait)."
+            )
+        )
+        element["options"]["orientation"] = "portrait"
+    }
+    return element
+}
+
+function toMM(length: string): number {
+    let value = parseFloat(length)
+    let unit = length.replace(value.toString(), "")
+    return convert(value).from(unit).to("mm")
+}
+
+function generatePDF(input: Object[]): jsPDF {
+    let meta: {
+        name: string
+        description: string
+        author: string
+        subject: string
+        keywords: string[]
+        copyStatus: "unknown" | "copyrighted" | "publicDomain"
+        notice: string
+        copyURL: string
+    } = {
+        name: "",
+        description: "",
+        author: "",
+        subject: "",
+        keywords: [],
+        copyStatus: "unknown",
+        notice: "",
+        copyURL: ""
+    }
+    let document: {
+        width: string
+        length: string
+        orientation: "portrait" | "landscape" | "p" | "l"
+        color: string
+        numberPages: boolean
+        numXPos: number
+        numYPos: number
+    } = {
+        width: "297mm",
+        length: "210mm",
+        orientation: "portrait",
+        color: "#FFFFFF",
+        numberPages: false,
+        numXPos: null,
+        numYPos: null
+    }
+    let currentPage = {
+        options: {},
+        content: ""
+    }
+    let pages: {
+        options: { [key: string]: string }
+        content: string
+    }[] = []
+
+    const docOptions = createDocument(input[1])["options"]
+    for (let key in docOptions) {
+        document[key] = docOptions[key]
+    }
+
+    const doc = new jsPDF({
+        orientation: document["orientation"],
+        unit: "mm",
+        format: [toMM(document["width"]), toMM(document["length"])]
+    })
+
+    for (let i = 1; i < input.length; i++) {
+        const element = input[i]
+        const tagName = element["tagName"]
+        switch (tagName) {
+            case "document":
+                console.error(
+                    chalk.red(
+                        "Error: document tag should be the first tag in the document."
+                    )
+                )
+                process.exit(1)
+                break
+            case "meta":
+                try {
+                    element["options"]["keywords"] =
+                        element["options"]["keywords"].split(",")
+                } catch (e) {}
+                if (
+                    !["unknown", "copyrighted", "publicDomain"].includes(
+                        element["options"]["copyStatus"]
+                    )
+                ) {
+                    console.error(
+                        chalk.red(
+                            "Error: Option copyStatus in tag meta must be one of 'unknown', 'copyrighted', or 'publicDomain'."
+                        )
+                    )
+                    process.exit(1)
+                }
+                for (let key in element["options"]) {
+                    meta[key] = element["options"][key]
+                }
+                if (element["content"] != "") {
+                    console.warn(
+                        chalk.yellow(
+                            "Warning: meta content is not empty. This content will be ignored."
+                        )
+                    )
+                }
+                break
+            case "page":
+                if (element["content"] != "") {
+                    console.warn(
+                        chalk.yellow(
+                            "Warning: page content is not empty. This content will be ignored."
+                        )
+                    )
+                }
+                let firstPage = pages.length > 0 ? false : true
+                for (let key in document) {
+                    if (key == "numberPages") key = "numberPage"
+                    currentPage["options"][key] = element["options"][key]
+                        ? element["options"][key]
+                        : document[key]
+                }
+
+                if (!firstPage) {
+                    pages.push(currentPage)
+                    doc.addPage(
+                        [
+                            toMM(currentPage["options"]["width"]),
+                            toMM(currentPage["options"]["length"])
+                        ],
+                        currentPage["options"]["orientation"]
+                    )
+                }
+                break
+        }
+    }
+    return doc
+}
+
+export default generatePDF
--- opus/index.ts
+++ opus/index.ts
@@ -1,6 +1,15 @@
 import parse from "./parse"
+import build from "./build"
 
 import { program } from "commander"
+import chalk from "chalk"
+
+import fs from "fs"
+
+function parseHrtimeToSeconds(hrtime) {
+    var seconds = (hrtime[0] + hrtime[1] / 1e9).toFixed(3)
+    return seconds
+}
 
 program
     .name("opus")
@@ -8,13 +17,28 @@ program
     .description("A fully customizable programmatic document processing system")
 
 program
-    .requiredOption("-i, --input <input>", "Input file")
+    .requiredOption("-i, --input <input>", "Input file path")
     .option("-o, --output <output>", "Output file", "<input>.pdf")
 
 program.parse()
 
 const options = program.opts()
+options.output = options.output.replace(
+    "<input>",
+    options.input.replace(".opus", "")
+)
+
+console.log("Building from", options.input, "\n")
+const startTime = process.hrtime()
 
 let parsed = parse(options.input)
+let built = build(parsed)
 
-console.log(parsed)
+built.save(options.output)
+
+const elapsedSeconds = parseHrtimeToSeconds(process.hrtime(startTime))
+console.log(
+    chalk.green(
+        `Build completed in ${elapsedSeconds} seconds. See output at ${options.output}`
+    )
+)
